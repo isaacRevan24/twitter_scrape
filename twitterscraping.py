b0e3deb6f55033d81fcfab868539408ebbb11856
isaacRevan24 <isaac2498atencio@criptext.com>
2020-04-19 17:34:38 -0500

    Elimina los tweets duplicados

--- twitterscraping.py
+++ twitterscraping.py
@@ -2,13 +2,15 @@ from twitterscraper import query_tweets
 import datetime as dt
 import pandas as pd
 
-begin_date = dt.date(2019, 12, 1)
+begin_date = dt.date(2020, 1, 1)
 end_date = dt.date(2020, 4, 1)
-limit = 1000
+limit = 1500
 lang = 'spanish'
 
-tweets = query_tweets("covid-19", begindate=begin_date,
+tweets = query_tweets(["panama", "coronavirus"], begindate=begin_date,
                       enddate=end_date, limit=limit, lang=lang)
 
 df = pd.DataFrame(t.__dict__ for t in tweets)
-df.to_csv('datosTest.csv', index=False)
+df.sort_values("timestamp", ascending = True, inplace=True)
+df.drop_duplicates("tweet_id", inplace=True)
+df.to_csv('twitter_data.csv', index=False)
