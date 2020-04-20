7868cbfe071f4c1eadb2029a558a0c69c36be6d1
isaacRevan24 <isaac2498atencio@criptext.com>
2020-04-19 20:37:59 -0500

    Agregado más busquedas de twitter

--- twitterscraping.py
+++ twitterscraping.py
@@ -4,13 +4,40 @@ import pandas as pd
 
 begin_date = dt.date(2020, 1, 1)
 end_date = dt.date(2020, 4, 1)
-limit = 1500
+limit = 1000000
 lang = 'spanish'
 
+# Busqueda "#coronavirus"
 tweets = query_tweets(["panama", "coronavirus"], begindate=begin_date,
                       enddate=end_date, limit=limit, lang=lang)
 
 df = pd.DataFrame(t.__dict__ for t in tweets)
 df.sort_values("timestamp", ascending = True, inplace=True)
 df.drop_duplicates("tweet_id", inplace=True)
-df.to_csv('twitter_data.csv', index=False)
+df.to_csv('twitter_dataset_1.csv', index=False)
+
+
+# Busqueda "#covid-19"
+tweets = query_tweets(["panama", "covid-19"], begindate=begin_date,
+                      enddate=end_date, limit=limit, lang=lang)
+
+df = pd.DataFrame(t.__dict__ for t in tweets)
+df.sort_values("timestamp", ascending = True, inplace=True)
+df.drop_duplicates("tweet_id", inplace=True)
+df.to_csv('twitter_dataset_2.csv', index=False)
+
+
+
+# Busqueda "#pandemia"
+tweets = query_tweets(["panama", "pandemia"], begindate=begin_date,
+                      enddate=end_date, limit=limit, lang=lang)
+
+df = pd.DataFrame(t.__dict__ for t in tweets)
+df.sort_values("timestamp", ascending = True, inplace=True)
+df.drop_duplicates("tweet_id", inplace=True)
+df.to_csv('twitter_dataset_3.csv', index=False)
+
+
+
+
+
